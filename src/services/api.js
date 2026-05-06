b787cd4922b0f14bfa7fdad75ea1c5e98ec52ef2
Thanikachalam <thanikatech@gmail.com>
2026-05-06 16:24:57 +0530

    Updated API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-backend-je9k.onrender.com",
 });
 
 // attach token automatically
