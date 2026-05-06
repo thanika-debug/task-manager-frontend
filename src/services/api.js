17e653ea773c22f64e9f9cffd7b9d629cae46c8e
Thanikachalam <thanikatech@gmail.com>
2026-05-06 16:42:20 +0530

    Fixed API baseURL and token format

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: "https://task-manager-backend-je9k.onrender.com",
+  baseURL: "https://task-manager-backend-je9k.onrender.com/api",
 });
 
 // attach token automatically
@@ -10,7 +9,7 @@ API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization = token;
+    req.headers.Authorization = `Bearer ${token}`;
   }
 
   return req;
